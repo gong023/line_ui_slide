29061f80179e5907dfb64eef44d14f41cdb31332
gong023 <gon.gong.gone@gmail.com>
2014-09-07 11:38:36 +0900

    create javascript base

--- js/slide.js
+++ js/slide.js
@@ -1,25 +1,54 @@
-//var slide = [
-//  {
-//    place: 'center',
-//    message: 'にこまきさんがログインしました'
-//  },
-//  {
-//    place: 'left',
-//    message: 'みなさんこんにちは'
-//  },
-//  {
-//    place: 'left',
-//    message: 'チームにこまきともうします'
-//  },
-//];
+var slide = [
+  {
+    place: 'info',
+    message: 'にこまきさんがログインしました'
+  },
+  {
+    place: 'author',
+    message: 'みなさんこんにちは'
+  },
+  {
+    place: 'me',
+    message: 'チームにこまきともうします'
+  },
+];
+
+var append_info = function(i, message) {
+  var element = "<tr id=slide_" + i + " style='display: none;'><td class='msg center-block text-center'><div class='msg-balloon'>" + message + "</div></td></tr>"
+  $("#info_message").append(element);
+}
+
+var append_author = function(i, message) {
+  var element = "<tr id=slide_" + i + " style='display: none;'><td><div class='msg-balloon'>" + message + "</div></td></tr>";
+  $("#author_message").append(element);
+}
+
+var append_me = function(i, message) {
+  var element = "<tr id=slide_" + i + " style='display: none;'><td class='col-msg'><div class='msg-balloon'>" + message + "</div></td></tr>";
+  $("#me_message").append(element);
+}
+
 
 var index = 0;
 $(function() {
-//  $('li').hide();
+
+  for(var i in slide) {
+    switch(slide[i].place) {
+      case 'info':
+        append_info(i, slide[i].message);
+        break;
+      case 'author':
+        append_author(i, slide[i].message);
+        break;
+      case 'me':
+        append_me(i, slide[i].message);
+        break;
+    }
+  }
 
   $('body').keypress(function(e) {
     if (e.which == 13) {
-      $("#" + index).show();
+      $("#slide_" + index).show();
       index += 1;
       return false;
     }
