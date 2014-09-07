ab4f268dc5273d7f8d98708d6854636c2e800a2d
gong023 <gon.gong.gone@gmail.com>
2014-09-07 10:23:31 +0900

    save commit

--- js/slide.js
+++ js/slide.js
@@ -1,26 +1,25 @@
-var slide = [
-  {
-    place: 'center',
-    message: 'にこまきさんがログインしました'
-  },
-  {
-    place: 'left',
-    message: 'みなさんこんにちは'
-  },
-  {
-    place: 'left',
-    message: 'チームにこまきともうします'
-  },
-];
+//var slide = [
+//  {
+//    place: 'center',
+//    message: 'にこまきさんがログインしました'
+//  },
+//  {
+//    place: 'left',
+//    message: 'みなさんこんにちは'
+//  },
+//  {
+//    place: 'left',
+//    message: 'チームにこまきともうします'
+//  },
+//];
 
 var index = 0;
 $(function() {
+//  $('li').hide();
+
   $('body').keypress(function(e) {
     if (e.which == 13) {
-      d = slide[index];
-      console.log(d);
-      var item = $("<li><div class='" + d.place +"'><div class='msg-balloon'>" + d.message + "</div></div></li>");
-      $("#content").append(item);
+      $("#" + index).show();
       index += 1;
       return false;
     }
